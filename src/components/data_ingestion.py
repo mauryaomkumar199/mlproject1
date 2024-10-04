5bb3a1adabba4c96703723dc9e2ca391e6c4f03f
[OMKUMAR MAURYA] <[mauryaomkumar199@gmail.com]>
2024-10-04 07:08:48 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -61,6 +61,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataingestionConfig:
     train_data_path: str = os.path.join("artifacts_1", "train.csv")
@@ -98,6 +101,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()  # Corrected method name
+    train_data,test_data=obj.initiate_data_ingestion()  # Corrected method name
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
                      
